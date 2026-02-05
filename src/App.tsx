496f3408f3d0b1c758721036fc065eb44251f341
Iain Soars <iain.soars@bca.com>
2026-02-05 08:52:43 +0000

    Added styling

--- src/App.tsx
+++ src/App.tsx
@@ -67,53 +67,54 @@ function App() {
     return (
     <div className="App">
         <h1>Transposition</h1>
+        <div className="panel">
+            <form id="form" onSubmit={handleSubmit}>
+                <h2>Source instrument</h2>
+                <select name="sourceInstrument" defaultValue="Bb">
+                    {TRANSPOSING_INSTRUMENTS.map((instrument) => (
+                        <option key={instrument} value={instrument}>{instrument} Trumpet</option>
+                    ))}
+                </select>
 
-        <form id="form" onSubmit={handleSubmit}>
-            <h2>Source instrument</h2>
-            <select name="sourceInstrument" defaultValue="Bb">
-                {TRANSPOSING_INSTRUMENTS.map((instrument) => (
-                    <option key={instrument} value={instrument}>{instrument} Trumpet</option>
-                ))}
-            </select>
+                <h2>Source key</h2>
+                <select name="sourceKey" defaultValue="C">
+                    <option value="C">C (no sharps or flats)</option>
+                    <option value="G">G (1 sharp)</option>
+                    <option value="D">D (2 sharps)</option>
+                    <option value="A">A (3 sharps)</option>
+                    <option value="E">E (4 sharps)</option>
+                    <option value="B">B (5 sharps)</option>
+                    <option value="F#">F# (6 sharps)</option>
+                    <option value="C#">C# (7 sharps)</option>
+                    <option value="F">F (1 flat)</option>
+                    <option value="Bb">Bb (2 flats)</option>
+                    <option value="Eb">Eb (3 flats)</option>
+                    <option value="Ab">Ab (4 flats)</option>
+                    <option value="Db">Db (5 flats)</option>
+                    <option value="Gb">Gb (6 flats)</option>
+                    <option value="Cb">Cb (7 flats)</option>
+                </select>
 
-            <h2>Source key</h2>
-            <select name="sourceKey" defaultValue="C">
-                <option value="C">C (no sharps or flats)</option>
-                <option value="G">G (1 sharp)</option>
-                <option value="D">D (2 sharps)</option>
-                <option value="A">A (3 sharps)</option>
-                <option value="E">E (4 sharps)</option>
-                <option value="B">B (5 sharps)</option>
-                <option value="F#">F# (6 sharps)</option>
-                <option value="C#">C# (7 sharps)</option>
-                <option value="F">F (1 flat)</option>
-                <option value="Bb">Bb (2 flats)</option>
-                <option value="Eb">Eb (3 flats)</option>
-                <option value="Ab">Ab (4 flats)</option>
-                <option value="Db">Db (5 flats)</option>
-                <option value="Gb">Gb (6 flats)</option>
-                <option value="Cb">Cb (7 flats)</option>
-            </select>
+                <h2>Source note</h2>
+                <select name="sourceNote" defaultValue="C">
+                    {CHROMATIC.map((note) => (
+                        <option key={note} value={note}>{note}</option>
+                    ))}
+                </select>
 
-            <h2>Source note</h2>
-            <select name="sourceNote" defaultValue="C">
-                {CHROMATIC.map((note) => (
-                    <option key={note} value={note}>{note}</option>
-                ))}
-            </select>
+                <button type="submit">Transpose</button>
 
-            <button type="submit">Transpose</button>
+                <h2>Target instrument</h2>
+                <select name="targetInstrument" defaultValue="Bb">
+                    {TRANSPOSING_INSTRUMENTS.map((instrument) => (
+                        <option key={instrument} value={instrument}>{instrument} Trumpet</option>
+                    ))}
+                </select>
 
-            <h2>Target instrument</h2>
-            <select name="targetInstrument" defaultValue="Bb">
-                {TRANSPOSING_INSTRUMENTS.map((instrument) => (
-                    <option key={instrument} value={instrument}>{instrument} Trumpet</option>
-                ))}
-            </select>
-
-            <h2>Transposed key: {transposedKey} ({transpositionInterval})</h2>
-            <h2>Transposed note: {transposedNote} ({transpositionInterval})</h2>
-        </form>
+                <h2>Transposed key: {transposedKey} ({transpositionInterval})</h2>
+                <h2>Transposed note: {transposedNote} ({transpositionInterval})</h2>
+            </form>
+        </div>
     </div>
     );
 }
