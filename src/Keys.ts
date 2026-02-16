49fc922b6c4b70bfd53071ca37e41961c0156c13
Iain Soars <iain@schweben.org>
2026-02-16 16:58:22 +0000

    Tidied MajorKey class

--- src/Keys.ts
+++ src/Keys.ts
@@ -1,47 +1,20 @@
-export const TRANSPOSING_INSTRUMENTS: string[] = [
-    "C",
-    "D",
-    "E♭",
-    "E",
-    "F",
-    "A",
-    "B♭",
-    "B"
-];
-
-export const INTERVALS: Record<number, string> = {
-    0: 'Unison',
-    1: 'Minor 2nd',
-    2: 'Major 2nd',
-    3: 'Minor 3rd',
-    4: 'Major 3rd',
-    5: 'Perfect 4th',
-    6: 'Augmented 4th',
-    7: 'Perfect 5th',
-    8: 'Minor 6th',
-    9: 'Major 6th',
-    10: 'Minor 7th',
-    11: 'Major 7th',
-    12: 'Octave'
-};
-
 export class MajorKey {
 
     static readonly C: MajorKey = new MajorKey('C', 0, 0, 'A');
-    static readonly G = new MajorKey('G', 1, 0, 'E');
-    static readonly D = new MajorKey('D', 2, 0, 'B');
-    static readonly A = new MajorKey('A', 3, 0, 'F♯');
-    static readonly E = new MajorKey('E', 4, 0, 'C♯');
-    static readonly B = new MajorKey('B', 5, 0, 'G♯');
-    static readonly F_SHARP= new MajorKey('F♯', 6, 0, 'D♯');
-    static readonly C_SHARP = new MajorKey('C♯', 7, 0, 'A♯', 'D♭');
-    static readonly F = new MajorKey('F', 0, 1, 'D');
-    static readonly B_FLAT = new MajorKey('B♭', 0, 2, 'G', 'A♯');
-    static readonly E_FLAT = new MajorKey('E♭', 0, 3, 'C', 'D♯');
-    static readonly A_FLAT = new MajorKey('A♭', 0, 4, 'F', 'G♯');
-    static readonly D_FLAT = new MajorKey('D♭', 0, 5, 'B♭', 'C♯');
-    static readonly G_FLAT = new MajorKey('G♭', 0, 6, 'E♭');
-    static readonly C_FLAT = new MajorKey('C♭', 0, 7, 'A♭');
+    static readonly G: MajorKey = new MajorKey('G', 1, 0, 'E');
+    static readonly D: MajorKey = new MajorKey('D', 2, 0, 'B');
+    static readonly A: MajorKey = new MajorKey('A', 3, 0, 'F♯');
+    static readonly E: MajorKey = new MajorKey('E', 4, 0, 'C♯');
+    static readonly B: MajorKey = new MajorKey('B', 5, 0, 'G♯');
+    static readonly F_SHARP: MajorKey = new MajorKey('F♯', 6, 0, 'D♯');
+    static readonly C_SHARP: MajorKey = new MajorKey('C♯', 7, 0, 'A♯', 'D♭');
+    static readonly F: MajorKey = new MajorKey('F', 0, 1, 'D');
+    static readonly B_FLAT: MajorKey = new MajorKey('B♭', 0, 2, 'G', 'A♯');
+    static readonly E_FLAT: MajorKey = new MajorKey('E♭', 0, 3, 'C', 'D♯');
+    static readonly A_FLAT: MajorKey = new MajorKey('A♭', 0, 4, 'F', 'G♯');
+    static readonly D_FLAT: MajorKey = new MajorKey('D♭', 0, 5, 'B♭', 'C♯');
+    static readonly G_FLAT: MajorKey = new MajorKey('G♭', 0, 6, 'E♭');
+    static readonly C_FLAT: MajorKey = new MajorKey('C♭', 0, 7, 'A♭');
 
     private readonly name: string;
     private readonly sharps: number;
@@ -57,23 +30,19 @@ export class MajorKey {
         this.enharmonic = enharmonic;
     }
 
-    public getName(): string {
+    private getName(): string {
         return this.name;
     }
 
-    public getEnharmonicName(): string {
+    private getEnharmonicName(): string {
         return this.enharmonic ? `${this.name}/${this.enharmonic}` : this.name;
     }
 
-    public getRelativeMinor(): string {
+    private getRelativeMinor(): string {
         return this.relativeMinor;
     }
 
-    public getEnharmonic(): string | undefined {
-        return this.enharmonic;
-    }
-
-    public getKeySignature(): string {
+    private getKeySignature(): string {
         if (this.sharps === undefined || this.flats === undefined) {
             return '';
         }
@@ -183,4 +152,31 @@ export class MajorKey {
             this.C.getRelativeMinor()
         ];
     }
-}
+};
+
+export const TRANSPOSING_INSTRUMENTS: string[] = [
+    "C",
+    "D",
+    "E♭",
+    "E",
+    "F",
+    "A",
+    "B♭",
+    "B"
+];
+
+export const INTERVALS: Record<number, string> = {
+    0: 'Unison',
+    1: 'Minor 2nd',
+    2: 'Major 2nd',
+    3: 'Minor 3rd',
+    4: 'Major 3rd',
+    5: 'Perfect 4th',
+    6: 'Augmented 4th',
+    7: 'Perfect 5th',
+    8: 'Minor 6th',
+    9: 'Major 6th',
+    10: 'Minor 7th',
+    11: 'Major 7th',
+    12: 'Octave'
+};
